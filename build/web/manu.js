25ad0351b1679c2ad08f72dd79fd9296736f8afe
leo566491 <wutunghan@WuTungs-MacBook-Air.local>
2016-04-11 16:14:20 +0800

    CP1406 min site assignment

---
+++ build/web/manu.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write(' <div class="header"><a href="index.html">Training Zone</a><div>');
+document.write(' <table bgcolor="#2EFEF7" border="3" style="width:100%" > <tr>');
+document.write(' <td><a href="home.html">Training Article</a></td>');
+document.write(' <td><a href="history.html">History</a></td>');
+document.write(' <td><a href="announce.html">Announcement</a></td></tr>');
+document.write(' <br/></div></table>');
---
+++ build/web/manu2.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write(' <link rel="stylesheet" type="text/css" href="style.css"/>');
+document.write(' <table bgcolor="#2EFEF7" border="1" style="width:100%"> <tr>');
+document.write(' <td><a href="contact.html">Contact Us</a></td>');
+document.write(' <td><a href="link.html">Links</a></td>');
+document.write(' </table></tr>');
